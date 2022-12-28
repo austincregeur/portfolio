a4cefa3302a4e214a2543e5383680d3db3fff302
acregeur <72456837+acregeur@users.noreply.github.com>
2022-12-28 13:02:09 -0500

    First Commit

---
+++ src/Nav.js
@@ -0,0 +1,18 @@
+import './Nav.css'
+
+function Nav() {
+
+    return (
+        <div className="flexbox-container">
+            <h1>Austin Cregeur</h1>
+            <h2>Full-Stack Developer</h2>
+            <h3>Resume</h3>
+            <a href="https://www.linkedin.com/in/acregeur/" target="_blank" rel="noreferrer"><h3>LinkedIn</h3></a>
+            <a href="https://github.com/austincregeur" target="_blank" rel="noreferrer"><h3>GitHub</h3></a>
+            <h3>Projects</h3>
+            <h3>Contact Me</h3>
+        </div>
+    );
+}
+
+export default Nav;
---
+++ src/Project.js
@@ -0,0 +1,19 @@
+import './Project.css'
+
+function Project() {
+    return (
+        <div className="project-container">
+            <h4>Personal Portfolio</h4>
+            <div className="project-description">
+                <h4>Image</h4>
+                <h4>Description</h4>
+            </div>
+            <div className="project-links">
+                <h4>Project Link</h4>
+                <h4>Code Link</h4>
+            </div>
+        </div>
+    );
+}
+
+export default Project;
